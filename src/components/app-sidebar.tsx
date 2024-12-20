a15acf4facfd3853b8721fea79052e138cbe51ca
sreehari2003 <sreeharivijaya2003@gmail.com>
2024-12-21 00:15:20 +0530

    feat: added dashboard ui

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { GalleryVerticalEnd, SquareTerminal } from "lucide-react";
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Credhive",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+  ],
+  navMain: [
+    {
+      title: "Changelog",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Create Changelog",
+          url: "#",
+        },
+        {
+          title: "Your Changelogs",
+          url: "#",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
